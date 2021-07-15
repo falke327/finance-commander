1fc58a1b2844edfd6b32a9aa584a3e0f927ae113
Florian Herrmann <florian.herrmann@isb-ag.de>
2021-07-15 17:34:45 +0200

    FC-001 Initial commit

---
+++ src/main/java/de/falke/financecommander/FinanceCommanderApplication.java
@@ -0,0 +1,13 @@
+package de.falke.financecommander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCommanderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceCommanderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/falke/financecommander/FinanceCommanderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.falke.financecommander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceCommanderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
