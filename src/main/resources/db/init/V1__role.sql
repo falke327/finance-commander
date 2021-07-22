fc971957fdcce0f2808c7778334dd2d76736687a
Florian Herrmann <herrmann.florian@mailbox.org>
2021-07-22 18:31:39 +0200

    FC-002 Simplified role creation due to beeing a manual step

--- src/main/resources/db/init/V1__role.sql
+++ src/main/resources/db/init/V1__role.sql
@@ -1,11 +1,1 @@
-DO
-$do$
-    BEGIN
-        IF NOT EXISTS (
-                SELECT FROM pg_catalog.pg_roles  -- SELECT list can be empty for this
-                WHERE  rolname = 'finance_commander') THEN
-
-            CREATE ROLE finance_commander LOGIN PASSWORD 'finance21';
-        END IF;
-    END
-$do$;
+CREATE ROLE finance_commander LOGIN PASSWORD 'finance21';
