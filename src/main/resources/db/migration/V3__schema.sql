7e634b028c938528a38d2b7a542b855d049363de
Florian Herrmann <herrmann.florian@mailbox.org>
2021-07-22 17:46:40 +0200

    FC-002 Get flyway to work

--- src/main/resources/db/migration/V3__schema.sql
+++ src/main/resources/db/migration/V3__schema.sql
@@ -1,8 +1,3 @@
-CREATE USER finance_commander with password 'finance21';
--- TODO: get better password if not used in local dev system
-
-CREATE DATABASE finance_commander OWNER finance_commander;
-
 CREATE SCHEMA IF NOT EXISTS finance_commander AUTHORIZATION finance_commander;
 
 ALTER SCHEMA finance_commander OWNER TO finance_commander;
