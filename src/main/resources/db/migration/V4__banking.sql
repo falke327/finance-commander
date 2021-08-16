aa325ef7742899dcdfa8d94012129cf137ba8caf
Florian Herrmann <herrmann.florian@mailbox.org>
2021-08-16 16:22:19 +0200

    FC-002 Cleaned up primary keys and added category group

--- src/main/resources/db/migration/V4__banking.sql
+++ src/main/resources/db/migration/V4__banking.sql
@@ -1,47 +1,58 @@
 CREATE TABLE finance_commander.bank
 (
-    bic       VARCHAR(20)  NOT NULL PRIMARY KEY,
-    bank_name VARCHAR(100) NOT NULL
+    bic       VARCHAR(20)  NOT NULL,
+    bank_name VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_bank PRIMARY KEY (bic)
 );
 
 CREATE TABLE finance_commander.account
 (
-    iban        VARCHAR(50)                                         NOT NULL PRIMARY KEY,
+    iban        VARCHAR(50)                                         NOT NULL,
     bank        VARCHAR(20) REFERENCES finance_commander.bank (bic) NOT NULL,
     description VARCHAR(255),
-    expires     DATE
+    expires     DATE,
+    CONSTRAINT pk_account PRIMARY KEY (iban)
 );
 
 CREATE TABLE finance_commander.banking_owner
 (
-    user_id    INTEGER      NOT NULL PRIMARY KEY,
+    user_id    SERIAL       NOT NULL,
     first_name VARCHAR(100) NOT NULL,
-    last_name  VARCHAR(100) NOT NULL
+    last_name  VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_banking_owner PRIMARY KEY (user_id)
 );
 
 CREATE TABLE finance_commander.account_to_owner
 (
     account_number VARCHAR(50) REFERENCES finance_commander.account (iban)      NOT NULL,
     banking_owner  INTEGER REFERENCES finance_commander.banking_owner (user_id) NOT NULL,
-    CONSTRAINT PK_ACCOUNT_TO_OWNER PRIMARY KEY (account_number, banking_owner)
+    CONSTRAINT pk_account_to_owner PRIMARY KEY (account_number, banking_owner)
+);
+
+CREATE TABLE finance_commander.category_group
+(
+    category_group_id   SERIAL       NOT NULL,
+    category_group_name VARCHAR(100) NOT NULL,
+    CONSTRAINT pk_category_group PRIMARY KEY (category_group_id)
 );
 
 CREATE TABLE finance_commander.category
 (
-    category_name  VARCHAR(100) NOT NULL PRIMARY KEY,
-    category_group VARCHAR(100) NOT NULL
+    category_id    SERIAL                                                                  NOT NULL,
+    category_name  VARCHAR(100)                                                            NOT NULL,
+    category_group INTEGER REFERENCES finance_commander.category_group (category_group_id) NOT NULL,
+    CONSTRAINT pk_category PRIMARY KEY (category_id)
 );
-
--- TODO: insert category_group
 
 CREATE TABLE finance_commander.transaction
 (
-    transaction_id      INTEGER                                                 NOT NULL PRIMARY KEY,
+    transaction_id      SERIAL                                                  NOT NULL,
     account_number      VARCHAR(50) REFERENCES finance_commander.account (iban) NOT NULL,
     value_date          DATE                                                    NOT NULL,
     applicant           VARCHAR(100)                                            NOT NULL,
     transaction_subject VARCHAR(255)                                            NOT NULL,
     transaction_value   NUMERIC(10, 2)                                          NOT NULL,
-    category            VARCHAR(100) REFERENCES finance_commander.category (category_name),
-    fixed               BOOLEAN
+    category            INTEGER REFERENCES finance_commander.category (category_id),
+    fixed               BOOLEAN,
+    CONSTRAINT pk_transaction PRIMARY KEY (transaction_id)
 );
